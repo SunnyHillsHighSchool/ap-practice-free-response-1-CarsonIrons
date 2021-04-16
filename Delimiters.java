3f0c16b3ec6c37d3860dbdd1a5c8d1b49d2717ac
Carson Irons <>
2021-04-16 16:27:26 +0000

    added comments and name/period

--- Delimiters.java
+++ Delimiters.java
@@ -11,27 +11,34 @@ public class Delimiters
 		closeDel = close;
 	}
 
+//Delimiter Methods
+//By Carson Irons P2
 	public ArrayList<String> getDelimitersList(String[] tokens)
 	{
+//create list for output
 	ArrayList<String> delimiters = new ArrayList<String>();
+  //traverse tokens
   for(int i=0;i<tokens.length;i++){
+//add delimiters to output list
 if(tokens[i].equals(openDel)||tokens[i].equals(closeDel))
 delimiters.add(tokens[i]);
   }
-  
+  //return delimiters 
   	return delimiters;
 	}
 
 	public boolean isBalanced(ArrayList<String> delimiters)
 	{ 
+ //create moderator 
     int count=0;
+    //traverse list and check for delimiters, use count to check for balance
 	for(int i=0;i<delimiters.size() && count>=0;i++){
 if(delimiters.get(i).equals(openDel))
 count++;
 if(delimiters.get(i).equals(closeDel))
 count--;
   }
-  
+  //return result
   	return count==0;
 	}
 }
